d88e22dbc3c8cc1fc10a43680b3fbe695baee3ee
Judith Retzlaff <Judith.Retzlaff@gmx.net>
2023-08-08 16:54:22 +0200

    fix WordList title

--- pages/words/index.js
+++ pages/words/index.js
@@ -3,13 +3,15 @@ import Heading from "@/components/PageHeading";
 import { MainContent } from "@/components/StyledComponents/MainContent";
 
 export default function WordList({ wordList, databaseIsLoading }) {
-  return (
-    <MainContent>
-      <Heading PageTitle={`${wordList.length} Saved Words`} />
-      <EntriesContainer
-        wordList={wordList}
-        databaseIsLoading={databaseIsLoading}
-      />
-    </MainContent>
-  );
+  if (wordList) {
+    return (
+      <MainContent>
+        <Heading PageTitle={`${wordList.length} Saved Words`} />
+        <EntriesContainer
+          wordList={wordList}
+          databaseIsLoading={databaseIsLoading}
+        />
+      </MainContent>
+    );
+  }
 }
