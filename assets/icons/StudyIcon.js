1a158f2a4e61cfe1929e35ea0904450b1c675841
Judith Retzlaff <Judith.Retzlaff@gmx.net>
2023-08-09 14:37:07 +0200

    icon size

--- assets/icons/StudyIcon.js
+++ assets/icons/StudyIcon.js
@@ -2,8 +2,8 @@ export default function StudyIcon() {
   return (
     <svg
       className="inherit-background-color"
-      width="60"
-      height="60"
+      width="40"
+      height="40"
       viewBox="0 0 60 60"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
