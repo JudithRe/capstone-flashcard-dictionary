fb170dd061def5cd2467137d4424fa86fcd93824
Judith Retzlaff <Judith.Retzlaff@gmx.net>
2023-08-23 09:52:48 +0200

    initial commit

--- fonts.js
+++ fonts.js
@@ -1,13 +1,11 @@
-import { Permanent_Marker, Poppins } from "@next/font/google/";
+import { Permanent_Marker, Poppins } from "next/font/google";
 
 export const permanent = Permanent_Marker({
   subsets: ["latin"],
   weight: ["400"],
-  variable: "--font-permanent",
 });
 
 export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400"],
-  variable: "--font-poppins",
 });
